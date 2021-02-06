26f181099afef4f306b7646dd04778a1440441b0
rcammisola <rocco.cammisola@performgroup.com>
2021-02-06 01:36:37 +0000

    Failing test for tied score games

--- tests/test_tennis_game.py
+++ tests/test_tennis_game.py
@@ -12,6 +12,28 @@ def test_score_is_love_all_at_start_of_game():
 @pytest.mark.parametrize(
     "player1_score, player2_score, expected_score",
     [
+        (0, 0, "Love-all"),
+        (1, 1, "Fifteen-all"),
+        (2, 2, "Thirty-all"),
+    ]
+)
+def test_tied_game_scenarios(player1_score, player2_score, expected_score):
+    player1_name = "Federer"
+    player2_name = "Nadal"
+    game = TennisGame(player1_name, player2_name)
+
+    for point in range(max(player1_score, player2_score)):
+        if point < player1_score:
+            game.won_point(player1_name)
+        if point < player2_score:
+            game.won_point(player2_name)
+
+    assert game.score() == expected_score
+
+
+@pytest.mark.parametrize(
+    "player1_score, player2_score, expected_score",
+    [
         (1, 0, "Fifteen-Love"),
         (0, 1, "Love-Fifteen"),
         (3, 1, "Forty-Fifteen"),
