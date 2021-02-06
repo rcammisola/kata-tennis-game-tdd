96002aa624b1a09311e159a3d83e896e802f6f38
rcammisola <rocco.cammisola@performgroup.com>
2021-02-06 01:27:09 +0000

    Test for multiple points scored situation (passes without further changes)

--- tests/test_tennis_game.py
+++ tests/test_tennis_game.py
@@ -25,3 +25,16 @@ def test_score_love_fifteen_when_player_2_scores_first_point():
     game.won_point(player2_name)
 
     assert game.score() == "Love-Fifteen"
+
+
+def test_score_forty_fifteen_when_players_have_scored_3_1():
+    player1_name = "Federer"
+    player2_name = "Nadal"
+    game = TennisGame(player1_name, player2_name)
+
+    game.won_point(player1_name)
+    game.won_point(player1_name)
+    game.won_point(player1_name)
+    game.won_point(player2_name)
+
+    assert game.score() == "Forty-Fifteen"
