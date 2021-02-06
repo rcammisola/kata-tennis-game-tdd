60a7c96efc13233e0db294369938b2a89ab62948
rcammisola <rocco.cammisola@performgroup.com>
2021-02-06 01:24:19 +0000

    Provide implementation to solve the 15-0 scenario

--- src/tennis_game.py
+++ src/tennis_game.py
@@ -4,9 +4,29 @@ class TennisGame:
     def __init__(self, player1_name, player2_name):
         self.player1_name = player1_name
         self.player2_name = player2_name
+        self.player1_points = 0
+        self.player2_points = 0
+
+        self.point_map = {
+            0: "Love",
+            1: "Fifteen",
+            2: "Thirty",
+            3: "Forty",
+        }
 
     def score(self):
-        return "Love-all"
+        if self.player1_points == self.player2_points:
+            return "Love-all"
+        else:
+            player1_score = self.point_map[self.player1_points]
+            player2_score = self.point_map[self.player2_points]
+
+            return f"{player1_score}-{player2_score}"
 
     def won_point(self, player_name):
-        pass
+        if player_name == self.player1_name:
+            self.player1_points += 1
+        elif player_name == self.player2_name:
+            self.player2_points += 1
+        else:
+            raise ValueError(f"Illegal player provided: '{player_name}'")
