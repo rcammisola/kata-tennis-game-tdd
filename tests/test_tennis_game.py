15abbd650af9c89886a42c0ce6dcd2667fc0e4fa
rcammisola <rocco.cammisola@performgroup.com>
2021-02-06 01:25:29 +0000

    Test that passes straight away for opposite scenario - player 2 scores first point

--- tests/test_tennis_game.py
+++ tests/test_tennis_game.py
@@ -15,3 +15,13 @@ def test_score_fifteen_love_when_player_1_scores_first_point():
     game.won_point(player1_name)
 
     assert game.score() == "Fifteen-Love"
+
+
+def test_score_love_fifteen_when_player_2_scores_first_point():
+    player1_name = "Federer"
+    player2_name = "Nadal"
+    game = TennisGame(player1_name, player2_name)
+
+    game.won_point(player2_name)
+
+    assert game.score() == "Love-Fifteen"
