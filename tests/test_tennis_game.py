8686fc6a987316413cb0974b6c9beb8086ab2d33
rcammisola <rocco.cammisola@performgroup.com>
2021-02-06 02:27:53 +0000

    start of game test no longer needed because tied game test handles this case

--- tests/test_tennis_game.py
+++ tests/test_tennis_game.py
@@ -3,12 +3,6 @@ import pytest
 from tennis_game import TennisGame
 
 
-def test_score_is_love_all_at_start_of_game():
-    game = TennisGame("Federer", "Nadal")
-
-    assert game.score() == "Love-all"
-
-
 @pytest.mark.parametrize(
     "player1_score, player2_score, expected_score",
     [
