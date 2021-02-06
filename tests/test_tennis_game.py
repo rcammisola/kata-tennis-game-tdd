7cef02a30e653b539b491f121813599ecaa9af41
rcammisola <rocco.cammisola@performgroup.com>
2021-02-06 01:17:08 +0000

    Failing test: test_score_fifteen_love_when_player_1_scores_first_point

--- tests/test_tennis_game.py
+++ tests/test_tennis_game.py
@@ -5,3 +5,13 @@ def test_score_is_love_all_at_start_of_game():
     game = TennisGame("Federer", "Nadal")
 
     assert game.score() == "Love-all"
+
+
+def test_score_fifteen_love_when_player_1_scores_first_point():
+    player1_name = "Federer"
+    player2_name = "Nadal"
+    game = TennisGame(player1_name, player2_name)
+
+    game.won_point(player1_name)
+
+    assert game.score() == "Fifteen-Love"
